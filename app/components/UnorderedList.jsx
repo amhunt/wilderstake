680e8c78e6988c4ab60de43586bbbf671c43193a
Glitch (wilderstake) <none>
2018-02-17 08:07:13 +0000

    🏍🚃 Checkpoint

--- app/components/UnorderedList.jsx
+++ app/components/UnorderedList.jsx
@@ -1,15 +1,33 @@
 const React = require('react');
 
 const explanationPoints = [
+  {
+    title: '1. Buy stake',
+    description: 'Buy stake for Eth so that you can participate in conservation elections.'
+  },
+  {
+    title: '2. Vote on land to preserve',
+    description: 'Propose and vote on plots of land that you find important to preserve.'
+  },
+  {
+    title: '3. We\'ll take care of the rest',
+    description: 'We complete the buy orders and add the land to our trust. If the buy is not possible, you\'ll get your tokens back!'
+  },
   
-  ]
+];
 
 /* takes an array prop 'items' and returns a <ul> element 
    with each item as <li> elements */
 const Explanation = function() {
   return (
-    <div className="row">
-      <div className="col-4-sm">
+    <div className="container">
+      <div className="row">
+        {explanationPoints.map(point => (
+          <div className="col-4-sm">
+            <div style={{ fontStyle: 'bold' }}>{point.title}</div>
+            <div>{point.description}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
--- public/bundle.js
+++ public/bundle.js
@@ -22443,14 +22443,35 @@ module.exports = Home;
 
 const React = __webpack_require__(24);
 
+const explanationPoints = [
+  {
+    title: '1. Buy stake',
+    description: 'Buy stake for Eth so that you can participate in conservation elections.'
+  },
+  {
+    title: '2. Vote on land to preserve',
+    description: 'Propose and vote on plots of land that you find important to preserve.'
+  },
+  {
+    title: '3. We\'ll take care of the rest',
+    description: 'We complete the buy orders and add the land to our trust. If the buy is not possible, you\'ll get your tokens back!'
+  },
+  
+];
+
 /* takes an array prop 'items' and returns a <ul> element 
    with each item as <li> elements */
 const Explanation = function() {
   return (
-    React.createElement("ul", null, 
-      items.map(function(item, i) {
-        return React.createElement("li", {key: i}, item);
-      })
+    React.createElement("div", {className: "container"}, 
+      React.createElement("div", {className: "row"}, 
+        explanationPoints.map(point => (
+          React.createElement("div", {className: "col-4-sm"}, 
+            React.createElement("div", {style: { fontStyle: 'bold'}}, point.title), 
+            React.createElement("div", null, point.description)
+          )
+        ))
+      )
     )
   );
 }
