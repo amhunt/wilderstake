854c8900020a1ec0c1468ed9b3e88a89bb143094
Glitch (wilderstake) <none>
2018-02-17 19:19:30 +0000

    🚓🐦 Checkpoint app/components/Vote.jsx:689483/107

--- app/components/Vote.jsx
+++ app/components/Vote.jsx
@@ -13,12 +13,12 @@ const properties = [
     name: 'Alaskan Wilderness',
     submitter: 'Jamie. L',
     whyDesc: 'This land means so much to me. It runs right by the river that I grew up next to. We used to raft down to Denver, but this property is now under risk of deforestation and mining. Please help me protect this sacred property!',
-    imageUrl: 'http://www.dronestagr.am/aerial-lake-meets-forest-chiang-mai-thailand/',
+    imageUrl: 'https://static1.squarespace.com/static/5324cb3ae4b0c5c3326a28c8/533e10bee4b0258f11587f51/55b4210de4b0b31e18d4ff29/1437868302698/echo-lake-ancient-forest-aerial.jpg',
     cost: '$67,000',
     votes: '1805',
   },
   {
-    name: 'Oregon Butte',
+    name: 'Oregon Butte, Echo Lake',
     submitter: 'Jamie. L',
     whyDesc: 'This land means so much to me. It runs right by the river that I grew up next to. We used to raft down to Denver, but this property is now under risk of deforestation and mining. Please help me protect this sacred property!',
     imageUrl: '',
@@ -37,11 +37,11 @@ const Vote = function() {
       </div>
       <div>
         {properties.map(property => (
-          <div style={{ padding: 16, borderColor: '#ddd', borderWidth: ' }} key={property.name} className="container">
-            <div className="row">
+          <div style={{ padding: 16, margin: 16, borderColor: '#ddd', borderWidth: 1, borderStyle: 'solid' }} key={property.name} className="container">
+            <div style={{ margin: 'auto', maxWidth: '100%' }} className="row">
               <div style={{ height: 300, backgroundImage: `url(${property.imageUrl})` }} className="col-sm-4">
               </div>
-              <div className="col-sm-8">
+              <div style={{ paddingTop: 12, fontSize: 17 }} className="col-sm-8">
                 <div>Name of submitter: {property.submitter}</div>
                 <div>Submitter's appeal: {property.whyDesc}</div>
                 <div>Property cost: {property.cost}</div>
