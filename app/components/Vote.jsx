03e29193992a773016f23f12071c4aa87236ccb7
Glitch (wilderstake) <none>
2018-02-17 23:01:30 +0000

    ⛱🎌 Checkpoint app/components/Vote.jsx:689483/257

--- app/components/Vote.jsx
+++ app/components/Vote.jsx
@@ -32,56 +32,64 @@ const properties = [
 const BLOCK_TIME_SEC = 15;
 
 /* the main page for the index route of this app */
-const Vote = function() {
-  if (typeof window.web3 !== 'undefined') {
-    console.log('using mist or metamask');
-    const newWeb3 = new Web3(window.web3.currentProvider);
-    this.web3 = newWeb3;
-  } else {
-    alert('Please install Mist or MetaMask to use Blockbin');
-  }
-  let remainingBlocks;
-  let timeRemainingStr;
-  if (this.web3) {
-    this.web3.eth.getBlockNumber((error, result) => {
-      remainingBlocks = this.web3.eth.currentBlock % 84000;
-      const estRemainingTimeSecs = remainingBlocks * BLOCK_TIME_SEC;
-      var date = new Date(null);
-      date.setSeconds(estRemainingTimeSecs);
-      timeRemainingStr = date.toISOString().substr(11, 8);
-    });
+class Vote extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      timeRemainingStr: null,
+    };
   }
   
-  return (
-    <div>
-      <div style={{ textAlign: 'center', paddingBottom: 20 }}>
-        <h2>What should we buy next?</h2>
-        <h3>Use your stake to vote:</h3>
-        {timeRemainingStr && (
-          <Estimate timeRemainingStr={timeRemainingStr} />
-        )}
-      </div>
+  render() {
+    if (typeof window.web3 !== 'undefined') {
+      console.log('using mist or metamask');
+      const newWeb3 = new Web3(window.web3.currentProvider);
+      this.web3 = newWeb3;
+    } else {
+      alert('Please install Mist or MetaMask to use Blockbin');
+    }
+    let remainingBlocks;
+    let timeRemainingStr;
+    if (this.web3) {
+      this.web3.eth.getBlockNumber((error, result) => {
+        remainingBlocks = result % 84000;
+        const estRemainingTimeSecs = remainingBlocks * BLOCK_TIME_SEC;
+        var date = new Date(null);
+        date.setSeconds(estRemainingTimeSecs);
+        this.setState( timeRemainingStr: date.toISOString().substr(11, 8)};
+      });
+    }
+  
+    return (
       <div>
-        {properties.map(property => (
-          <div style={{ padding: 16, margin: '16px auto', width: '90%', borderColor: '#ddd', borderWidth: 1, borderStyle: 'solid' }} key={property.name} className="container">
-            <div style={{ margin: 'auto', maxWidth: '100%' }} className="row">
-              <div style={{ height: 300, backgroundSize: 'cover', backgroundImage: `url(${property.imageUrl})` }} className="col-sm-4" />
-              <div style={{ fontSize: 17 }} className="col-sm-8">
-                <div style={{ fontWeight: 600, paddingTop: 12, fontSize: 22 }}>
-                  {property.name}
+        <div style={{ textAlign: 'center', paddingBottom: 20 }}>
+          <h2>What should we buy next?</h2>
+          <h3>Use your stake to vote:</h3>
+          {timeRemainingStr && (<Estimate timeRemainingStr={timeRemainingStr} />)}
+        </div>
+        <div>
+          {properties.map(property => (
+            <div style={{ padding: 16, margin: '16px auto', width: '90%', borderColor: '#ddd', borderWidth: 1, borderStyle: 'solid' }} key={property.name} className="container">
+              <div style={{ margin: 'auto', maxWidth: '100%' }} className="row">
+                <div style={{ height: 300, backgroundSize: 'cover', backgroundImage: `url(${property.imageUrl})` }} className="col-sm-4" />
+                <div style={{ fontSize: 17 }} className="col-sm-8">
+                  <div style={{ fontWeight: 600, paddingTop: 12, fontSize: 22 }}>
+                    {property.name}
+                  </div>
+                  <div>Name of submitter: {property.submitter}</div>
+                  <div>Submitter's appeal: {property.whyDesc}</div>
+                  <div>Property cost: {property.cost}</div>
+                  <div>Current votes: {property.votes}</div>
+                  <Button style={{ position: 'absolute' }}>Vote</Button>
                 </div>
-                <div>Name of submitter: {property.submitter}</div>
-                <div>Submitter's appeal: {property.whyDesc}</div>
-                <div>Property cost: {property.cost}</div>
-                <div>Current votes: {property.votes}</div>
-                <Button style={{ position: 'absolute' }}>Vote</Button>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 module.exports = Vote;
